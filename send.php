a828169395b8df7f3172c24e6b88873332f44384
--global <jaka.pozun2@gmail.com>
2016-03-12 17:24:48 +0100

    Dela MAILER

--- send.php
+++ send.php
@@ -1,38 +1,34 @@
 <?php
 if(isset($_POST)){
     
-$vsebina = $_POST['vsebina'];
-$zadeva = $_POST['zadeva'];
-$uporabnik = $_POST['uporabnik'];
+$body = $_POST['vsebina'];
+$subject = $_POST['zadeva'];
+$to = $_POST['uporabnik'];
+    
+    $to = strstr($to, '|', true);
 }
 
-
-
- $to = $user['mail'];
-
-
-                require_once('phpmailer/class.phpmailer.php');
+                require_once('PHPMailer/PHPMailerAutoload.php');
 
                 $mail = new PHPMailer();  // create a new object
                 $mail->IsSMTP(); // enable SMTP
                 $mail->CharSet = 'utf-8';
-                $mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only
+                $mail->SMTPDebug = 1;  // debugging: 1 = errors and messages, 2 = messages only
                 $mail->SMTPAuth = true;  // authentication enabled
-                $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+                $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for GMail
                 $mail->Host = 'smtp.gmail.com';
-                $mail->Port = 465;
-                $mail->Username = "jaka.pozun"; //  SMTP username
-                $mail->Password = "" // SMTP password           
-                $mail->SetFrom("jaka.pozun@gmail.com", "Turistika");
+                $mail->Port = 587;
+                $mail->Username = "urban2.kovacic@gmail.com"; //  SMTP username
+                $mail->Password = "urbankovacic"; // SMTP password           
+                $mail->SetFrom("urban2.kovacic@gmail.com", "Turistika");
                 $mail->Subject = $subject;
                 $mail->Body = $body;
                 $mail->AddAddress($to);
-                if (!$mail->Send()) {
-                    $error = 'Mail error: ' . $mail->ErrorInfo;
-                } else {
-                    $error = 'Message sent!';
-
-                    header("Location: index.php");
-                    die();
-                }
+                if(!$mail->send()) {
+                        echo 'Message was not sent.';
+                        echo 'Mailer error: ' . $mail->ErrorInfo;
+                    } else {
+                        echo 'Message has been sent.';
+                    header ("Location: sporocila.php");
+                    }
 ?> 
--- sporocila.php
+++ sporocila.php
@@ -4,21 +4,20 @@ include_once 'database.php';
 
 ?>
 <?php
-$sql = "SELECT  u.first_name, u.last_name FROM users u ";
+$sql = "SELECT u.email, u.first_name, u.last_name FROM users u ";
 $result = mysqli_query($link, $sql);
 ?>                                           
-<form action="send.php" method="post">
+<form action="send.php" method="POST">
    <?php 
     echo "<select name='uporabnik'>";
     while ($row = mysqli_fetch_array($result)) {
-        echo "<option name=$id>" . $row['first_name'] . " " . $row['last_name'] . "</option>";
+        echo "<option name=$id>" . $row['email'] . " | " . $row['first_name'] . " " . $row['last_name'] . "</option>";
     }
     echo "</select>";
 
     ?>
     ZADEVA: <input type="text" name="zadeva" /><br />
     <textarea style="resize:none" name="vsebina"  rows="5" cols="10" placeholder="Vpiši besedilo...">
-        
     </textarea>
     <input type="submit" value="POŠLJI" name="submit" />
 </form>
